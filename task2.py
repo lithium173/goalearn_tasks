7bb2ad1053faa9811f684ef02d2d61a9f57ee46b
lithium173 <62837619+lithium173@users.noreply.github.com>
2022-12-15 07:31:38 -0800

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,7 +6,6 @@ import math
 df = pd.read_csv('C:\\Users\\rozhansystem\\Documents\\salary.csv')
 
 dic = {'zero': 0,'one': 1,'two':2,'three':3, 'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'ten':10,'eleven':11,'twelve':12,'thirteen':13,'fourteen':14,'fifteen':15,'sixteen':16,'seventeen':17,'eighteen':18,'nineteen':19,'twenty':20}
-df = pd.read_csv('C:\\Users\\rozhansystem\\Documents\\salary.csv')
 
 
 
@@ -18,35 +17,69 @@ for ele in df['experience']:
         df['experience'] = df['experience'].replace(ele,dic[ele])# /Replacing name of number's with number's
 
 
-x = df[['experience','test_score(out of 10)','interview_score(out of 10)']]
+x = df[['experience','test_score','interview_score']]
 y = df['salary']
 
 
 model = linear_model.LinearRegression()
 model.fit(x,y)
 
-m = model.coef_
-b = model.intercept_
 
 
-def gradient_descent(x, y):
-    #initial value of m and b
-    m_curr = b_curr = 0
-    #initialize number of steps
-    iterations = 1000
-    #Number of data points n
-    n = len(x)
-    #Initialize learning rate
-    learning_rate = 0.001
+
+def prediction(exp,test,inter):
+    return model.predict([[exp,test,inter]])
+
+m1 ,m2 , m3 , b = 0 ,0 ,0 ,0 # m is coefficient & b is intercept
+
+iterations = 100000000000000000000 # num of iterations
+
+learning_rate = 0.0001  # default learning rate
+
+n = 8 # num of data's in dataframe
+exp = [] # list of experience
+test = [] # list of test score's
+interview = [] # list of interview score's
+ans = [] # list of salaries
+exp = df.experience
+test = df.test_score
+interview = df.interview_score
+ans = df.salary
+
+def MSE(): # Mean Squared Error
     
-    for i in range(iterations):
-        y_pred = m_curr * x + b_curr
-        cost = (1/n) * sum([val**2 for val in (y-y_pred)])
-        md = -(2/n)*sum(x*(y-y_pred))
-        bd = -(2/n)*sum(y-y_pred)
-        m_curr = m_curr - learning_rate * md
-        b_curr = b_curr - learning_rate * bd
-        print("m {}, b {}, cost {}, iteration {}".format(m_curr, b_curr, cost, i))
+    sum = 0
+    for i in range(n):
+        y_predict = prediction(exp[i],test[i],interview[i])
+        sum += (ans[i]-y_predict[0])**2
+    return sum
 
-gradient_descent(x,y)
-#incomplete
+
+def md_eq(exp):
+    sum = 0
+    for i in range(n):
+        y_predict = prediction(exp[i],test[i],interview[i])
+        sum += exp[i]*(ans[i]-y_predict[0])
+    return sum
+def b_eq():
+    sum = 0
+    for i in range(n):
+        y_predict = prediction(exp[i],test[i],interview[i])
+        sum += ans[i]-y_predict[0]
+    return sum
+
+for i in range(iterations):
+    cost = (1/n) * MSE()
+    md1 = -(2/n)* md_eq(exp)
+    md2 = -(2/n)* md_eq(test)
+    md3 = -(2/n)* md_eq(interview)
+    bd = -(2/n)* b_eq()
+    m1 = m1 - learning_rate * md1
+    m2 = m2 - learning_rate * md2
+    m3 = m3 - learning_rate * md3
+    b = b - learning_rate * bd
+    print("m1 {}, m2 {}, m3 {}, b {}, cost {}, iteration {}".format(m1, m2, m3, b, cost, i))
+
+m_orgin = model.coef_
+b_orgin = model.intercept_
+print(m_orgin,'\n',b_orgin)
